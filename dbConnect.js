4c9b1c84dae20d0bbf19657a52ce4736e83635b4
abdo <45274635+abdo290@users.noreply.github.com>
2022-03-07 22:14:30 +0200

    Update dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const URL = 'mongodb+srv://abdo290:Abdo1379@cluster0.xjx9e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const URL = 'mongodb://abdo290:Abdo1379@cluster0-shard-00-00.xjx9e.mongodb.net:27017,cluster0-shard-00-01.xjx9e.mongodb.net:27017,cluster0-shard-00-02.xjx9e.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-d3o2ax-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 mongoose.connect(URL)
 
